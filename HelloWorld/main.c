7915a36d3b5264bbc303b0508b4a85953c861280
Keith Samson <k.samson@klab.com>
2014-06-04 10:02:35 +0800

    Commiting my first C file

---
+++ HelloWorld/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by Keith Samson on 6/4/14.
+//  Copyright (c) 2014 Keith Samson. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
